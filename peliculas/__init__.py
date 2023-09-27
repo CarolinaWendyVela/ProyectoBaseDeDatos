474c881f4ae736d211697176054ef8907a9c4b30
CarolinaWendyVela <velacarolina52@gmail.com>
2023-09-27 10:44:36 -0300

    ProyectoPeliculas

--- peliculas/__init__.py
+++ peliculas/__init__.py
@@ -29,8 +29,15 @@ def create_app(test_config=None):
     def hello():
         return 'Hola mi mundo'
 
-
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
